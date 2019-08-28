ba1667d7e769956358ff214f8c1fdf7320c38c09
serkankayaa <srknnkaya@gmail.com>
2019-08-28 12:25:29 +0300

    edited games

--- scripts/balloon.js
+++ scripts/balloon.js
@@ -1,68 +1,134 @@
 $(document).ready(function () {
+    // var game1 = {
+    //     operator: '-', //Plus = +, Minus = -, Divide = /, Multiply = * 
+    //     resultNumber: 6,
+    //     balloonImage: 'img/balloons3.png',
+    //     balloonCount: 3,
+    //     balloonHeight: 140,
+    //     balloonWidth: 120,
+    //     internalPosition: -5,
+    //     balloonValues: [9, 5, 3]
+    // };
+
+    // var game2 = {
+    //     operator: '+', //Plus = +, Minus = -, Divide = /, Multiply = * 
+    //     resultNumber: 101,
+    //     balloonImage: 'img/balloons3.png',
+    //     balloonCount: 4,
+    //     balloonHeight: 140,
+    //     balloonWidth: 120,
+    //     internalPosition: -5,
+    //     balloonValues: [96, 16, 5, 7]
+    // };
+
+    // var game3 = {
+    //     operator: '/', //Plus = +, Minus = -, Divide = /, Multiply = * 
+    //     resultNumber: 2,
+    //     balloonImage: 'img/balloons3.png',
+    //     balloonCount: 4,
+    //     balloonHeight: 140,
+    //     balloonWidth: 120,
+    //     internalPosition: -5,
+    //     balloonValues: [58, 2, 20, 4]
+    // };
+
+    // var game4 = {
+    //     operator: '*', //Plus = +, Minus = -, Divide = /, Multiply = * 
+    //     resultNumber: 120,
+    //     balloonImage: 'img/balloons3.png',
+    //     balloonCount: 5,
+    //     balloonHeight: 140,
+    //     balloonWidth: 120,
+    //     internalPosition: -5,
+    //     balloonValues: [12, 5, 2, 21, 7]
+    // };
+
+    // var game5 = {
+    //     operator: '+', //Plus = +, Minus = -, Divide = /, Multiply = * 
+    //     resultNumber: 55,
+    //     balloonImage: 'img/balloons3.png',
+    //     balloonCount: 6,
+    //     balloonHeight: 140,
+    //     balloonWidth: 120,
+    //     internalPosition: -5,
+    //     balloonValues: [22, 35, 7, 33, 7, 5]
+    // };
+
+    // var game6 = {
+    //     operator: '*', //Plus = +, Minus = -, Divide = /, Multiply = * 
+    //     resultNumber: 400,
+    //     balloonImage: 'img/balloons3.png',
+    //     balloonCount: 5,
+    //     balloonHeight: 140,
+    //     balloonWidth: 120,
+    //     internalPosition: -5,
+    //     balloonValues: [20, 20, 2, 21, 17]
+    // };
+
     var game1 = {
-        operator: '-', //Plus = +, Minus = -, Divide = /, Multiply = * 
-        resultNumber: 6,
+        operator: '+', //Plus = +, Minus = -, Divide = /, Multiply = * 
+        resultNumber: 2,
         balloonImage: 'img/balloons3.png',
         balloonCount: 3,
         balloonHeight: 140,
         balloonWidth: 120,
         internalPosition: -5,
-        balloonValues: [9, 5, 3]
+        balloonValues: [1, 1, 3]
     };
 
     var game2 = {
         operator: '+', //Plus = +, Minus = -, Divide = /, Multiply = * 
-        resultNumber: 101,
+        resultNumber: 3,
         balloonImage: 'img/balloons3.png',
         balloonCount: 4,
         balloonHeight: 140,
         balloonWidth: 120,
         internalPosition: -5,
-        balloonValues: [96, 16, 5, 7]
+        balloonValues: [2, 16, 1, 7]
     };
 
     var game3 = {
-        operator: '/', //Plus = +, Minus = -, Divide = /, Multiply = * 
-        resultNumber: 2,
+        operator: '+', //Plus = +, Minus = -, Divide = /, Multiply = * 
+        resultNumber: 4,
         balloonImage: 'img/balloons3.png',
         balloonCount: 4,
         balloonHeight: 140,
         balloonWidth: 120,
         internalPosition: -5,
-        balloonValues: [58, 2, 20, 4]
+        balloonValues: [2, 2, 20, 4]
     };
 
     var game4 = {
-        operator: '*', //Plus = +, Minus = -, Divide = /, Multiply = * 
-        resultNumber: 120,
+        operator: '+', //Plus = +, Minus = -, Divide = /, Multiply = * 
+        resultNumber: 3,
         balloonImage: 'img/balloons3.png',
         balloonCount: 5,
         balloonHeight: 140,
         balloonWidth: 120,
         internalPosition: -5,
-        balloonValues: [12, 5, 2, 21, 7]
+        balloonValues: [1, 5, 2, 21, 7]
     };
 
     var game5 = {
         operator: '+', //Plus = +, Minus = -, Divide = /, Multiply = * 
-        resultNumber: 55,
+        resultNumber: 4,
         balloonImage: 'img/balloons3.png',
         balloonCount: 6,
         balloonHeight: 140,
         balloonWidth: 120,
         internalPosition: -5,
-        balloonValues: [22, 35, 7, 33, 7, 5]
+        balloonValues: [2, 35, 2, 33, 7, 5]
     };
 
     var game6 = {
-        operator: '*', //Plus = +, Minus = -, Divide = /, Multiply = * 
-        resultNumber: 400,
+        operator: '+', //Plus = +, Minus = -, Divide = /, Multiply = * 
+        resultNumber: 3,
         balloonImage: 'img/balloons3.png',
         balloonCount: 5,
         balloonHeight: 140,
         balloonWidth: 120,
         internalPosition: -5,
-        balloonValues: [20, 20, 2, 21, 17]
+        balloonValues: [1, 20, 2, 21, 17]
     };
 
     $('.container').css({
@@ -162,7 +228,6 @@ $(document).ready(function () {
 
                 if (check) {
                     selectedBalloons = selectedBalloons.filter(onlyUnique);
-
                     return;
                 }
 
